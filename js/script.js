5bbe1a79b7fbccbf51578dc97bd05ad993c6cf21
Jasper <jkolp@csumb.edu>
2019-11-13 15:17:49 -0800

    Progress

--- js/script.js
+++ js/script.js
@@ -9,10 +9,13 @@
 	// When changing character input
 		$("#character").on("change", function() {
 			var characterName = $("#character").val().toLowerCase().replace(/\s/g,'');
-			console.log(characterName);
+			let seriesInfo = [];
 
-			$("#list_of_comics").html("<h3> List of series related to this character </h3>");
+			console.log("Character Name : " + characterName);
+
+			$("#list_of_comics").append("<h3> List of series related to this character </h3>");
 			$("#list_of_comics").append("<h4>Character Name : " + characterName + "</h4>\n\n");
+			$("#list_of_comics").append('<ul id="series-list"></ul>\n\n');
 
 			$.ajax({
 				url : "https://gateway.marvel.com:443/v1/public/characters?name=" + characterName + "&ts=" + ts + "&apikey=74e2999625dc86ce7eb5f9dd6e589b17&hash=" + hash,// + "&limit=" + limit + "&offset=" + offset,
@@ -21,27 +24,61 @@
 
 					var charID = result.data.results[0].id
 
-					console.log(charID);
+					console.log("Character ID : " + charID);
 
 					var offset = 0;
+					let seriesInfo = [];
 					for(var j = 0; j < 10; j++)
 					{
 						$.ajax({
 							url : "https://gateway.marvel.com:443/v1/public/characters/"+charID+"/series?&limit=100&offset=" + offset + "&ts=" + ts + "&apikey=74e2999625dc86ce7eb5f9dd6e589b17&hash=" + hash,
 							dataType : "json",
 							success : function(result, status){
-								var seriesInfo = result.data.results
+								//console.log(result.data)
 
-								for (let i = 0; i < seriesInfo.length; i++)
-								{
-									console.log(seriesInfo[i].title)
-									$("#list_of_comics").append("<p>" + seriesInfo[i].title + "</p>")
+								var total = result.data.total
+								var dataObject = result.data
+								//console.log(dataObject)
+
+								// Limit is 100. Need to iterate through 100 objects
+								for(var i = 0; i < 100 ; i++)
+								{	
+										if (dataObject.count != 0)
+										{
+											seriesInfo.push(dataObject.results[i] );
+											console.log("Date Object : " +  dataObject.results[i].title);
+											$("#series-list").append('<li>' + dataObject.results[i].title + '</li>')
+										} 
 								}
+
+									//var seriesInfo = result.data.results
+									//console.log(result.data);
+									
+								/*
+								for (let i = 0; i < total; i++)
+								{
+									//console.log(seriesInfo[i].title)
+									//$("#table").append('<tr><td>' + seriesInfo[i].title + '</td></tr>')
+									$("#table").append(seriesInfo[i].title + '\n')
+								}
+								*/
 							}
 						})
 						offset += 100;
 					}
 				}
-			})
+				
 
-		})			
+			})
+			/*
+				console.log(seriesInfo.length);
+
+				for(var i = 0; i < seriesInfo.length ; i++)
+				{
+					console.log(seriesInfo.length);
+				}
+				*/
+
+		})		
+
+	
